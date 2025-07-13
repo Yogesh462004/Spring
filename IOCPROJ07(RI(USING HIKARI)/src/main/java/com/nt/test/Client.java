596390c82236ad554c188aaaeb394b8b3a0aebeb
Yogesh <yp@gmail.com>
2025-07-13 16:21:06 +0530

    hikari

---
+++ IOCPROJ07(RI(USING HIKARI)/src/main/java/com/nt/test/Client.java
@@ -0,0 +1,26 @@
+package com.nt.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.controller.Controller1;
+import com.nt.model.Customer;
+
+public class Client {
+
+	public static void main(String[] args) {
+       Customer cust=new Customer();
+       cust.setName("Mohan");
+       cust.setAmount(1000);
+       cust.setBill(2000);
+       cust.setDiscount(100);
+       ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+       Controller1 con=ctx.getBean("cont",Controller1.class);
+       try {
+    	   String result=con.registerCust(cust);
+    	   System.out.println(result);
+       }catch(Exception e) {
+    	   e.printStackTrace();
+       }
+	}
+
+}
