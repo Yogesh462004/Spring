2c9ddf096699e9487a3471b6bd52f1d70b754764
Yogesh <yp@gmail.com>
2025-07-15 11:00:41 +0530

    awareinjection

---
+++ IOCPROJ15AwareInjection+DependencyLookup/src/main/java/com/nt/beans/Cricketer.java
@@ -0,0 +1,22 @@
+package com.nt.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource(value="classpath:com/nt/commons/myfile.properties")
+public class Cricketer {
+  @Value("${per.jersey}")	
+  private int jersey;
+  @Value("${per.name}")
+  private String name;
+  private ApplicationContext ctx;
+  public void batting() {
+	  ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+       Bat b=ctx.getBean("bat",Bat.class);
+       System.out.println(b.runs()+" hi");
+  }
+}
