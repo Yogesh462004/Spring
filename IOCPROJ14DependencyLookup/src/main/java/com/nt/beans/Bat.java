36a0609172bf04f41ce9b19665e1c4b4cadbfcdf
Yogesh <yp@gmail.com>
2025-07-15 10:59:25 +0530

    dependencylookup

---
+++ IOCPROJ14DependencyLookup/src/main/java/com/nt/beans/Bat.java
@@ -0,0 +1,13 @@
+package com.nt.beans;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bat {
+	public int runs() {
+		return 700;
+	}
+
+}
